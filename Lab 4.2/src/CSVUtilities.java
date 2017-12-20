918c224972252171172be5ce65e41f2b348d5cde
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-12-20 13:10:40 -0500

    .

--- Lab 4.2/src/CSVUtilities.java
+++ Lab 4.2/src/CSVUtilities.java
@@ -1,39 +1,81 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CSVUtilities 
 {
 	private ArrayList<String> CSVData = new ArrayList<String>();
 	private int numColumns;
-	private File csv;
 	
-	public CSVUtilities(File csv)
+	public CSVUtilities(File csv) throws IOException
 	{
-		this.csv = csv;
-		this.numColumns = 12;
+		FileReader csvF = new FileReader(csv);
+		
+		try (BufferedReader br = new BufferedReader(csvF))
+		{
+			String line = br.readLine();
+			while (line != null)
+			{
+				String[] attributes = line.split(","); 
+				this.CSVData.addAll(Arrays.asList(attributes));
+				line = br.readLine();
+			}
+		}
+		catch (IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
 	}
 	
 	public List<String> getColumnHeaders()
 	{
-		for (int i = 0; i < numColumns; i++)
+		ArrayList<String> header = new ArrayList<String>();
+		String[] headers = CSVData.get(0).split(",");
+		for (int i = 0; i < headers.length; i++)
 		{
-			
+			header.add(headers[i]);
 		}
+		return header;
 	}
 	
-	public List<String> getData(int column)
+	public List<String> getDataString(int column)
 	{
-		
+		ArrayList<String> data = new ArrayList<String>();
+		for (String i : CSVData)
+		{
+			String[] k = i.split(",");
+			data.add(k[column - 1]);
+		}
+		return data;
 	}
 	
-	public List<Integer> getData(int column)
+	public List<Integer> getDataInteger(int column)
 	{
-		
+		ArrayList<Integer> data = new ArrayList<Integer>();
+		int track = 0;
+		for (String i : CSVData)
+		{
+			String[] k = i.split(",");
+			data.add(Integer.parseInt(k[track]));
+			track++;
+		}
+		return data;
 	}
 	
-	public List<Double> getData(int column)
+	public List<Double> getDataDouble(int column)
 	{
-		
+		ArrayList<Double> data = new ArrayList<Double>();
+		int track = 0;
+		for (String i : CSVData)
+		{
+			String[] k = i.split(",");
+			data.add(Double.parseDouble(k[track]));
+			track++;
+		}
+		return data;
 	}
 }
--- Lab 4.2/src/Runner.java
+++ Lab 4.2/src/Runner.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
 
 public class Runner 
 {
-
+	public static void main(String[] args)
+	{
+		
+	}
 }
